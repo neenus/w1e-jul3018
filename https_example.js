4de00b9b4eca93ecd13c03ed1637a8cfcba9622e
Neenus Gabriel <neenus@live.ca>
2018-08-06 03:39:08 +0000

    added simple https request from www.example.com

---
+++ https_example.js
@@ -0,0 +1,20 @@
+// Require https module
+var https = require('https');
+console.log('I did it!');
+// ========================================
+
+// make simple request from www.example.org
+var options = {
+    host: 'www.example.org',
+    path: '/'
+};
+
+// called by https when the request is made
+var callback = function() {
+    console.log('In response handler callback!');
+};
+
+console.log("I'M about to make the request!");
+
+https.request(options, callback).end();
+console.log("I've made the request");
